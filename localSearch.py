110ee7c05aa8c7e3de38b117ebedc7d1e7e3fc42
joshua.rinaldi <jarinaldi94@gmail.com>
2021-06-10 17:48:31 -0400

    finished local search - need to fix errors

--- localSearch.py
+++ localSearch.py
@@ -2,18 +2,19 @@
 # Written by: Joshua Rinaldi
 # For: AFIT/ENG CSCE 686, Spring 2021 Final Project
 
+from os import name
 import random
 from copy import deepcopy
 
 class Neighbor(object):
-    #TODO define
     def __init__(self, geneLen):
         self.gene = []
         for _ in range(geneLen):
-            self.gene.append(0)
+            self.gene.append(1)
         self.vertices = dict[int, Vertex]
         self.edges = dict[int, Edge]
-        self.fitness
+        self.fitness = None
+        self.alteredAllele = None
 
     def __repr__(self):
         return self.gene
@@ -92,41 +93,189 @@ def initializeGraph():
 
     return (vertices, edgeNodes, targetNodes, edges)
 
-def generateSeed(vertices: dict[int, Vertex], edges: dict[int, Edge]):
-    #TODO implement
-    return
+def graphReduction(seed: Neighbor, edgesToRemove: list[int]):
+    # pull the vertice and edge list into convenience variables
+    verts: dict[int, Vertex] = seed.vertices
+    edges: dict[int, Edge] = seed.edges
 
-def generateNeighbors(curSol: Neighbor, vertices: dict[int, Vertex], edges: dict[int, Edge]):
+    while len(verts) > 2 and len(edgesToRemove) > 0 and len(edges) > 1:
+        # pop first eID from list of edges to remove (there should only be one value here after the seed is created)
+        eID = edgesToRemove.pop(0)
+
+        # pull relevant info into more convenience variables
+        edgeR: Edge = edges[eID]
+        srcVert: Vertex = deepcopy(edgeR.source)
+        destVert: Vertex = deepcopy(edgeR.dest)
+
+        # create new vertex name - just increment the largest vertex name by one
+        vNames = []
+        for key in list(seed.vertices.keys()):
+            vNames.append(key)
+        vNames = sorted(vNames, reverse=True)
+        newVertName = vNames.pop(0)+1
+
+        # update edge values, also remove any self edges
+        selfEdgeKeys = []
+        for e in edges.keys():
+            changeSource = False
+            changeDest = False
+            if edges[e].source.name == srcVert.name or destVert.name: changeSource = True
+            if edges[e].dest.name == srcVert.name or destVert.name: changeDest = True
+
+            if changeSource:
+                edges[e].source.name = newVertName
+            if changeDest:
+                edges[e].dest.name = newVertName
+            if changeSource and changeDest:
+                selfEdgeKeys.append(e)
+
+        for key in selfEdgeKeys:
+            edges.pop(key)
+
+        # create the needed values for the seed
+        newNumTargs = srcVert.numTargs + destVert.numEdges
+        newNumEdges = srcVert.numEdges + destVert.numEdges
+        newNumVulns = srcVert.numVulns + destVert.numVulns
+        newNeighborList = srcVert.neighborList + destVert.neighborList
+        newNeighborList = list(set(newNeighborList))
+        while srcVert.name in newNeighborList:
+            newNeighborList.remove(srcVert.name)
+        while destVert.name in newNeighborList:
+            newNeighborList.remove(destVert.name)
+
+        # build the new seed with the values create above
+        newVert: Vertex = Vertex(newVertName, newNumVulns, newNeighborList, "S")
+        newVert.numEdges = newNumEdges
+        newVert.numTargs = newNumTargs
+
+        # remove old vertices from the vertex list, add new verex to the list
+        verts.pop(srcVert.name)
+        verts.pop(destVert.name)
+        verts[newVertName] = newVert
+
+    # in case of initial seed value - any edges not removed should be added back in
+    # update other values and return seed to caller
+    for eID in edgesToRemove:
+        seed.gene[eID -1] = 1
+    seed.edges = edges
+    seed.vertices = verts
+    return seed
+
+def evaluate(neigh: Neighbor, added: bool, numEdgeNodes, numTargNodes):
+    usingVertIdx= random.choice(list(neigh.vertices.keys()))
+    usingVert = neigh.vertices[usingVertIdx]
+    edge_delta = usingVert.numEdges / numEdgeNodes
+    targ_delta = usingVert.numTargs / numTargNodes
+
+    if added:
+        neigh.fitness = -1 + abs(edge_delta - targ_delta)
+    elif not added:
+        neigh.fitness = 1 + abs(edge_delta - targ_delta)
+
+    return neigh
+
+def generateSeed(vertices: dict[int, Vertex], edges: dict[int, Edge]):
+    # intialize the seed for the algo
+    geneLen = len(edges)
+    seed: Neighbor = Neighbor(geneLen)
+
+    numFlip = random.randint(0,(0.25 * geneLen))
+
+    # to start, randomly remove no more than a quarter of the edges from the graph
+    temp = geneLen - 1
+    a = random.randint(0,temp)
+    edgesToRemove = []
+    for _ in range(numFlip):
+        while seed.gene[a] == 0:
+            a = random.randint(0,temp)
+        seed.gene[a] = 0
+        edgesToRemove.append(a+1)
+
+    # insert the complete list of verts and edges into the seed
+    seed.vertices = deepcopy(vertices)
+    seed.edges = deepcopy(edges)
+
+    random.shuffle(edgesToRemove)
+    seed = graphReduction(seed, edgesToRemove)
+
+    return seed
+
+def generateNeighbors(curSol: Neighbor, vertices: dict[int, Vertex], edges: dict[int, Edge], numEdgeNodes, numTargNodes):
     #TODO implement
-    return
+    neighborhood = []
+
+    # loop over the gene, creating a new neighbor for every possible allele change that could happen
+    for idx in range(len(curSol.gene)):
+        # initialize the new neighbor with the shift in genes
+        newNeigh: Neighbor = Neighbor(len(curSol.gene))
+        newNeigh.gene = deepcopy(curSol.gene)
+        newNeigh.vertices = deepcopy(vertices)
+        newNeigh.edges = deepcopy(edges)
+
+        # change the appropriate allele
+        added = False
+        if newNeigh.gene[idx] == 0:
+            newNeigh.gene[idx] = 1
+            added = True
+        elif newNeigh.gene[idx] == 1: newNeigh.gene[idx] = 0
+        # this will help with tabu list
+        newNeigh.alteredAllele = idx
+
+        # determine edges to remove and perform graph reduction on newNeigh
+        edgesToRemove = []
+        for a in range(len(newNeigh.gene)):
+            if newNeigh.gene[a] == 0:
+                edgesToRemove.append(a+1)
+        newNeigh = graphReduction(newNeigh, edgesToRemove)
+
+        newNeigh = evaluate(newNeigh, added, numEdgeNodes, numTargNodes)
+
+        neighborhood.append(newNeigh)
+
+    neighborhood.sort(key=lambda n: n.fitness, reverse=True)
+
+    return neighborhood
 
 
 def main():
     (vertices, edgeNodes, targetNodes, edges) = initializeGraph()
 
     curSol = generateSeed(vertices, edges)
-    
-    noImprovement = False
+    curSol = evaluate(curSol, False, len(edgeNodes), len(targetNodes))
+
     split = False
     noImprovementIdx = 0
-    tabuList: dict[Neighbor, int]
+    tabuList: dict[int, int] = {}
+    # this is what will determine how long a value must live in the tabu list
+    # tabu lenght will be four provided a sufficient number of edges
+    tabuLength = 0.25 * len(edges)
+    if tabuLength > 4: tabuLength = 4
+    
     curFittest = curSol
 
+    firstIteration = True
+
     while noImprovementIdx < 10 and split == False:
-        neighborhood = generateNeighbors(curSol, vertices, edges)
-        nextSol = neighborhood.pop(0)
-        while nextSol in tabuList:
+        # decrement time each move has remaining in tabu list & remove from list if it has stayed long enough
+        if not firstIteration:
+            #BUG need to change where popping the keys from the dictionary - can't do inside loop
+            for key in tabuList.keys():
+                tabuList[key] -= 1
+                if tabuList[key] <= 0:
+                    tabuList.pop(key)
+        neighborhood: list[Neighbor] = generateNeighbors(curSol, vertices, edges, len(edgeNodes), len(targetNodes))
+        nextSol:Neighbor = neighborhood.pop(0)
+        while nextSol.alteredAllele in list(tabuList.keys()):
             nextSol = neighborhood.pop(0)
-        if nextSol.fitness > curFittest.fittest:
+        if nextSol.fitness > curFittest.fitness:
             curFittest = nextSol
             noImprovementIdx = -1
-        #TODO update tabu list
+        tabuList[nextSol.alteredAllele] = tabuLength
         curSol = nextSol
         if len(curSol.vertices) == 2:
             split = True
         noImprovementIdx += 1
-        if noImprovementIdx >= 10:
-            noImprovement = True
+        firstIteration = False
 
 
 
